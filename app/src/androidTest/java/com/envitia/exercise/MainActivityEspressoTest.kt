4e050e44ede035bf20675c41a1cf022a71ebdb9a
Peter Szollar <peter@Peters-MBP.home>
2024-05-01 21:04:34 +0100

    Adds blank text test

--- app/src/androidTest/java/com/envitia/exercise/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/envitia/exercise/MainActivityEspressoTest.kt
@@ -1,10 +1,12 @@
 package com.envitia.exercise
 
+import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -12,6 +14,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,13 +23,20 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class MainActivityEspressoTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    private lateinit var decorView: View
+
+    @Before
+    fun setUp() {
+        activityRule.scenario.onActivity { activity ->
+            decorView = activity.window.decorView
+        }
+    }
 
     @Test
     fun testAddingTextToList() {
@@ -40,6 +51,14 @@ class MainActivityEspressoTest {
         onView(withId(R.id.button)).perform(click())
         onView(withId(R.id.recycler_view))
             .check(matches(hasDescendant(withText("[$currentTime] $testText"))))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testBlankText() {
+        onView(withId(R.id.button)).perform(click())
+        onView(withText(R.string.no_text))
+            .inRoot(withDecorView(Matchers.not(decorView)))
             .check(matches(isDisplayed()))
     }
 }
